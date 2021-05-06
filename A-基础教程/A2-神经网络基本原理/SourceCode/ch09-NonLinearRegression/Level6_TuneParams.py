d5d922e1c1cc6e4fdb65965af7afb8b6ed0b4077
Yan Zong <yan@yzong.com>
2021-05-06 08:55:13 +0100

    Updated: 非常棒的参数调优的一些总结

--- A-基础教程/A2-神经网络基本原理/SourceCode/ch09-NonLinearRegression/Level6_TuneParams.py
+++ A-基础教程/A2-神经网络基本原理/SourceCode/ch09-NonLinearRegression/Level6_TuneParams.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 from HelperClass2.NeuralNet_2_0 import *
 
-train_data_name = "../../Data/ch09.train.npz"
-test_data_name = "../../Data/ch09.test.npz"
+train_data_name = "../../SourceCode/Data/ch09.train.npz"
+test_data_name = "../../SourceCode/Data/ch09.test.npz"
 
 def train(hp, folder):
     net = NeuralNet_2_0(hp, folder)
@@ -57,47 +57,109 @@ if __name__ == '__main__':
     
     folder = "complex_turn"
 
-    ne, batch, eta = 4, 10, 0.1
+    # ne, batch, eta = 4, 10, 0.1
+    # file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 10, 0.3
+    # file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
+    
+    # ne, batch, eta = 4, 10, 0.5
+    # file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 10, 0.7
+    # file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
+    
+    # ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
+    
+    # ne, batch, eta = 4, 5, 0.1
+    # file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 10, 0.1
+    # file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
+
+    # # already have this data
+    # ne, batch, eta = 4, 15, 0.1
+    # file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 20, 0.1
+    # file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
+    
+    # ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
+
+    # ne, batch, eta = 2, 10, 0.5
+    # file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 10, 0.5
+    # file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 6, 10, 0.5
+    # file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 8, 10, 0.5
+    # file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
+
+
+    ne, batch, eta = 4, 2, 0.1
     file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
 
-    ne, batch, eta = 4, 10, 0.3
-    file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
-    
-    ne, batch, eta = 4, 10, 0.5
-    file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
-
-    ne, batch, eta = 4, 10, 0.7
-    file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
-    
-    ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
-    
-    ne, batch, eta = 4, 5, 0.5
-    file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
-
-    ne, batch, eta = 4, 10, 0.5
+    ne, batch, eta = 4, 5, 0.1
     file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
 
     # already have this data
-    ne, batch, eta = 4, 15, 0.5
+    ne, batch, eta = 4, 10, 0.1
     file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
 
-    ne, batch, eta = 4, 20, 0.5
+    ne, batch, eta = 4, 15, 0.1
     file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
     
     ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
 
-    ne, batch, eta = 2, 10, 0.5
-    file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
 
-    ne, batch, eta = 4, 10, 0.5
-    file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
+    # ne, batch, eta = 4, 2, 0.3
+    # file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
 
-    ne, batch, eta = 6, 10, 0.5
-    file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
+    # ne, batch, eta = 4, 5, 0.3
+    # file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
 
-    ne, batch, eta = 8, 10, 0.5
-    file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
+    # # already have this data
+    # ne, batch, eta = 4, 10, 0.3
+    # file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
 
-    ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
+    # ne, batch, eta = 4, 15, 0.3
+    # file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
+    
+    # ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
 
 
+    # ne, batch, eta = 4, 2, 0.5
+    # file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 5, 0.5
+    # file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
+
+    # # already have this data
+    # ne, batch, eta = 4, 10, 0.5
+    # file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 15, 0.5
+    # file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
+    
+    # ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)
+    
+ 
+    # ne, batch, eta = 4, 2, 0.7
+    # file_1, hp1 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 5, 0.7
+    # file_2, hp2 = try_hyperParameters(folder, ne, batch, eta)
+
+    # # already have this data
+    # ne, batch, eta = 4, 10, 0.7
+    # file_3, hp3 = try_hyperParameters(folder, ne, batch, eta)
+
+    # ne, batch, eta = 4, 15, 0.7
+    # file_4, hp4 = try_hyperParameters(folder, ne, batch, eta)
+    
+    # ShowLossHistory(folder, file_1, hp1, file_2, hp2, file_3, hp3, file_4, hp4)    
