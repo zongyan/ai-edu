ea76bb2d0cb446b4627f14cd58c02ae7437af066
Yan Zong <yan@yzong.com>
2021-05-17 17:24:34 +0100

    Updated: 梯度检查（gradients check）

--- A-基础教程/A2-神经网络基本原理/SourceCode/ch12-MultipleLayerNetwork/Level2_GradientCheck.py
+++ A-基础教程/A2-神经网络基本原理/SourceCode/ch12-MultipleLayerNetwork/Level2_GradientCheck.py
@@ -18,12 +18,16 @@ def dictionary_to_vector(dict_params):
         if count == 0:
             theta = new_vector
         else:         #np.concatenate
-            theta = np.concatenate((theta, new_vector), axis=0)
+            theta = np.concatenate((theta, new_vector), axis=0) 
+            # theta的结构是一个Nx1的向量，然后向量的结构是按照如下的顺序
+            # W1, B1, W2, B2, W3, B3， 对于W1这样的矩阵，就是会每一行一行的进行
+            # 转换成向量的形式
         count = count + 1
  
     return theta, keys
 
 # roll all grad values into one vector, the same shape as dictionary_to_vector()
+# 主要思路和dictionary_to_vector()是一样的
 def gradients_to_vector(gradients):
     count = 0
     for key in ["dW1", "dB1", "dW2", "dB2", "dW3", "dB3"]:
@@ -82,7 +86,18 @@ if __name__ == '__main__':
     hp = HyperParameters_3_0(n_input, n_hidden1, n_hidden2, n_output, eta, max_epoch, batch_size, eps, NetType.MultipleClassifier, InitialMethod.Xavier)
     net = NeuralNet_3_0(hp, "MNIST_gradient_check")
     dict_Param = {"W1": net.wb1.W, "B1": net.wb1.B, "W2": net.wb2.W, "B2": net.wb2.B, "W3": net.wb3.W, "B3": net.wb3.B}
-
+    '''
+    input layer: 7; hidden layer 1: 16; hidden layer 2: 12; output layer: 10
+    这里的说，都是可以理解为每一层neuron的个数（对于输入层有点不准确，但主要是为了
+    表达清楚概念）。
+    每一层有多少个neuron，就是有多少个bias B的了，所以，就是对于dict_Param来说，
+    B1， B2， B3 (对应hidden & output layer)的维度就是16， 12， 10。
+    但是呢，对于weights W的理解，就是不能够像这么简单的了，具体点说，
+    对于hidden layer 1的weights来说，input layer的第一个neuron就是会和hidden layer 1
+    的所有neurons相连接（1 x 16），input layer的第二个neuron就是会和hidden layer 1
+    的所有neurons相连接（1 x 16），以此类推，所以，W1的维度是7x16， W2的维度是16x12， 
+    W3的维度是12x10
+    '''
     layer_dims = [n_input, n_hidden1, n_hidden2, n_output]
     n_example = 2
     x = np.random.randn(n_example, n_input)
@@ -93,7 +108,11 @@ if __name__ == '__main__':
     net.forward(x)
     net.backward(x, y)
     dict_Grads = {"dW1": net.wb1.dW, "dB1": net.wb1.dB, "dW2": net.wb2.dW, "dB2": net.wb2.dB, "dW3": net.wb3.dW, "dB3": net.wb3.dB}
-
+    # dict_Grads 和 dict_Param的维度是一样的，参见上面的解释。
+    
+    """
+    因为W和B是矩阵，而不是一个标量，需要先是向量化 
+    """    
     J_theta, keys = dictionary_to_vector(dict_Param)
     d_theta_real = gradients_to_vector(dict_Grads)
 
@@ -107,6 +126,19 @@ if __name__ == '__main__':
         J_theta_plus = np.copy(J_theta)
         J_theta_plus[i][0] = J_theta[i][0] + eps
         # 多分类交叉熵
+        """
+        这里就是需要使用更新之后的weights & biases，计算这个loss的数值。这个理解是
+        正确的，就是加上一个数值eps（或者减去一个数值），得到更新之后的weights & biases，
+        然后这个更新后的weights & biases来计算loss function的数值。 
+        
+        每一次增加或减去eta，都是仅仅对model中的一个weight进行的，然后经过range(n)
+        次之后，就是得到了一个完成的d_theta_approx了。
+        
+        
+        如图12.4，纵轴y就是每一次得到的loss function，而横轴，仅仅是model中的一个
+        weight或者bias（x轴不是所有的参数，而仅仅是NN中的一个参数，比如说，如果网络
+        中有N个weights & biases，那么图12.4就是应该是需要N张图）。
+        """
         J_plus[i] = CalculateLoss(net, vector_to_dictionary(J_theta_plus, layer_dims), x, y, n_example)
 
         J_theta_minus = np.copy(J_theta)
