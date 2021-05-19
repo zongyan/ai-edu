7e36547d04c3a6f9928c21c227d55535096ce485
Yan Zong <yan@yzong.com>
2021-05-19 17:24:44 +0100

    Updated: 迷你神经网络框架的搭建； PyTorch框架简单示意图

--- A-基础教程/A2-神经网络基本原理/SourceCode/ch14-DnnBasic/Level1_ch09.py
+++ A-基础教程/A2-神经网络基本原理/SourceCode/ch14-DnnBasic/Level1_ch09.py
@@ -4,8 +4,8 @@
 from MiniFramework.NeuralNet_4_0 import *
 from MiniFramework.ActivationLayer import *
 
-train_file = "../../Data/ch09.train.npz"
-test_file = "../../Data/ch09.test.npz"
+train_file = "../../SourceCode/Data/ch09.train.npz"
+test_file = "../../SourceCode/Data/ch09.test.npz"
 
 def ShowResult(net, dr):
     fig = plt.figure(figsize=(12,5))
@@ -57,6 +57,18 @@ def model():
     fc2 = FcLayer_1_0(num_hidden1, num_output, params)
     net.add_layer(fc2, "fc2")
 
+    """
+    上面这一段代码中，之所以使用这个add_layer的形式，就是为了避免大量的重复性的代码，
+    作者就是使用了add_layer的function来完成这个步骤的，具体点说，是用了layer_list和
+    layer_name两个list变量，然后就是可以把layer的层数和layer的名字对应上的了，然后
+    每次添加一个layer，就是使用append即可。
+    所以，每次就是先是初始化每一层的input，output，weights&bias等参数，然后就是使用
+    add_layer这个函数就是可以把每一层连接成为一个neural network。
+    
+    另外，就是在这个training的时候，比如__forward， __backward， __update等函数就是
+    使用的是for loop的形式，逐层进行计算，得到output，gradient，更新的weight&bias数值
+    """
+    
     net.train(dataReader, checkpoint=100, need_test=True)
 
     net.ShowLossHistory()
