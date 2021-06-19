17c6876cceeb574f83d6a8b0fabe2894795e1d49
Yan Zong <yan@yzong.com>
2021-06-19 22:48:32 +0100

    Ensemble Learning有Boosting & bagging两大类方式； bagging的实现， 以及aggregation的三类算法； numpy.vstack(), numpy.argmax, mumpy.bincount的用法

--- A-基础教程/A2-神经网络基本原理/SourceCode/ch16-DnnRegularization/Level7_BaggingLearner.py
+++ A-基础教程/A2-神经网络基本原理/SourceCode/ch16-DnnRegularization/Level7_BaggingLearner.py
@@ -38,6 +38,8 @@ def train(dataReader):
     return net
 
 if __name__ == '__main__':
+    """这里就是定义了九个神经网络，每一个神经网路都是对应一个特定的数据集的（不是同一个
+    数据集，同时使用了shuffle来进行不同的排序的），这个数据集里面，也是会产生validation set"""
     nets = []
     net_count = 9
     for i in range(net_count):
@@ -45,9 +47,11 @@ if __name__ == '__main__':
         net = train(dataReader)
         nets.append(net)
     #end for
+    
     # test
+    """然后这里就是对9个trained model进行inference，采用的数据集是index=0的数据集"""    
     test_count = dataReader.num_test
-    dataReader = LoadData(0)
+    dataReader = LoadData(0) # 调用了index=0的数据集，但是我还是很好奇，就是对这个数据就有没有什么要求呢
     predict_array = None
     for i in range(net_count):
         test_x, test_y = dataReader.GetBatchTestSamples(test_count, 0)
@@ -56,14 +60,15 @@ if __name__ == '__main__':
         if i == 0:
             predict_array = predict
         else:
-            predict_array = np.vstack((predict_array, predict))
+            predict_array = np.vstack((predict_array, predict)) # Stack arrays in sequence vertically (row wise)
         # end if
     # end for
 
     # vote
+    """然后这里就是根据不同的aggregation算法进行整合之前的数据的了。"""    
     ra = np.zeros(test_count)
     for i in range(test_count):
-        ra[i] = np.argmax(np.bincount(predict_array[:,i]))
+        ra[i] = np.argmax(np.bincount(predict_array[:,i])) # 这两个command的解释，就是参见section 16.7了
 
     ry = np.argmax(test_y, axis=1)
     r = (ra == ry)
--- A-基础教程/A2-神经网络基本原理/SourceCode/ch16-DnnRegularization/Level7_BootstrappingGenerator.py
+++ A-基础教程/A2-神经网络基本原理/SourceCode/ch16-DnnRegularization/Level7_BootstrappingGenerator.py
@@ -15,9 +15,9 @@ def GenerateDataSet(subfolder, count=10):
     mdr.ReadLessData(1000)
     
     for i in range(count):
-        X = np.zeros_like(mdr.XTrainRaw)
-        Y = np.zeros_like(mdr.YTrainRaw)
-        list = np.random.choice(1000,1000)
+        X = np.zeros_like(mdr.XTrainRaw) # 生成类似于XTrainRaw维度的array
+        Y = np.zeros_like(mdr.YTrainRaw) # 生成类似于YTrainRaw维度的array
+        list = np.random.choice(1000,1000) # 第一个1000是范围，第二个1000是size
         k=0
         for j in list:
             X[k] = mdr.XTrainRaw[j]
